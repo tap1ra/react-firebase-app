38f2e1d89b837482ba4f81fe114f976c07e8ae9f
Yasuaki Tahira <yasuaki.tahira@gmail.com>
2019-12-29 00:03:04 +0900

    init crawler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,24 @@
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
+import puppeteer from 'puppeteer';
+
+import { feedCalendar } from './crawlers/kodansha-calendar';
+import { saveFeedMemo } from './firestore-admin/feed-memo';
+
 import { collectionName } from './services/mangarel/constants';
+
+const PUPPETEER_OPTIONS = {
+  args: [
+    '--disable-gpu',
+    '-–disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+    '--single-process',
+  ],
+  headless: true,
+};
 
 admin.initializeApp();
 export const publishers = functions
@@ -12,4 +30,24 @@ export const publishers = functions
       .get();
     const data = snap.docs.map(doc => doc.data());
     res.send({ data });
+  });
+
+export const fetchCalendar = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '2GB',
+  })
+  .pubsub.schedule('0 2 1,10,20 * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async () => {
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
+    const page = await browser.newPage();
+    const db = admin.firestore();
+
+    const memos = await feedCalendar(page);
+    const fetchCount = await saveFeedMemo(db, memos, 'kodansha');
+
+    await browser.close();
+    console.log(`Fetched Kodansha calendar. Wrote ${fetchCount} memos.`);
   });
